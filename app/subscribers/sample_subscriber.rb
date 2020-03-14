ac409ebe037fd3abf982c087c98bfd3c3e634bcc
soartec-lab <info@soartec-lab.work>
2020-03-15 00:05:12 +0900

    Add SampleSubscriber

---
+++ app/subscribers/sample_subscriber.rb
@@ -0,0 +1,21 @@
+class SampleSubscriber
+  include ActiveSupport
+  
+  Notifications.subscribe('event.library') do |*args|
+    event = ActiveSupport::Notifications::Event.new *args
+   
+    event.name
+    event.payload
+   
+    Rails.logger.info "#{event.name} Received!"
+  end
+
+  Notifications.subscribe('output_logger.sample.SampleSubscriber') do |*args|
+    event = ActiveSupport::Notifications::Event.new *args
+   
+    event.name
+    event.payload 
+   
+    Rails.logger.info "#{event.name} Received!"
+  end
+end
